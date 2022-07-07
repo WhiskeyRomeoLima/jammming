c8b89a9b6bf7535f16e15a9c4c92a44713e83e49
WhiskeyRomeoLima <wrlucas67@gmail.com>
2022-07-07 12:12:56 -0500

    added App, SearchBox Components

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div class="App">
+        Add a SearchBar component
+        <div className="App-playlist">Add a SearchResults component Add a Playlist component</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
